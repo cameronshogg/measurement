0d27cd4f4c334635618d50a29fbc6f8efb0352f7
Cameron Sequeira-Hogg <cameron@Camerons-MacBook-Pro.local>
2022-01-26 17:03:00 -0500

    added variance stuff

--- measurement.R
+++ measurement.R
@@ -1,5 +1,20 @@
 measurement <- read.csv("measurement.csv", header=T)
 
+# Variance Stuff
+
+part_one = subset(measurement, part == 1)
+var(part_one$y300)
+
+part_two = subset(measurement, part == 2)
+var(part_two$y300)
+
+part_three = subset(measurement, part == 3)
+var(part_three$y300)
+
+#SD 
+sqrt(var(part_one$y300+part_two$y300+part_three$y300)/3)
+
+
 # Measurement Part - currently rigged for 18 measurements per part, at once
 
 measurement$part <- c(rep(1, 18), rep(2, 18), rep(3, 18))
@@ -15,3 +30,5 @@ boxplot(measurement$y300~measurement$part,
 # boxplot(measurement$y300~measurement$day, 
 #         main = "Boxplot of Camshaft Straightness by Part",
 #         xlab = "Part", ylab = "Camshaft Straightness", col = "royalblue1")
+
+

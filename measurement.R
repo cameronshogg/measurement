48b187bcd47f3aba54f908ce6bd9b7b62d7f06f3
Cameron Sequeira-Hogg <cameron@Camerons-MacBook-Pro.local>
2022-01-26 16:35:11 -0500

    added files

---
+++ measurement.R
@@ -0,0 +1,17 @@
+measurement <- read.csv("measurement.csv", header=T)
+
+# Measurement Part - currently rigged for 18 measurements per part, at once
+
+measurement$part <- c(rep(1, 18), rep(2, 18), rep(3, 18))
+
+boxplot(measurement$y300~measurement$part, 
+        main = "Boxplot of Camshaft Straightness by Part",
+        xlab = "Part", ylab = "Camshaft Straightness", col = "aquamarine3")
+
+# Measurement Day - use if we stratify over each day, 20 measurements per part over a week, 4 per part over a day
+
+# measurement$day <- c(rep(c(rep(1, 4), rep(2, 4), rep(3, 4), rep(4, 4)),3))
+# 
+# boxplot(measurement$y300~measurement$day, 
+#         main = "Boxplot of Camshaft Straightness by Part",
+#         xlab = "Part", ylab = "Camshaft Straightness", col = "royalblue1")

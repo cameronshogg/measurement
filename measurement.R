440b76313f62c2fa583f12ec51ad4c2531eec4d5
Cameron Sequeira-Hogg <cameronsequeirahogg@gmail.com>
2022-01-28 12:21:29 -0500

    removed time family stuff

--- measurement.R
+++ measurement.R
@@ -16,6 +16,7 @@ sqrt(var(part_one$y300+part_two$y300+part_three$y300)/3)
 
 
 # Measurement Part - currently rigged for 18 measurements per part, at once
+# Using sample data set
 
 measurement$part <- c(rep(1, 18), rep(2, 18), rep(3, 18))
 
@@ -23,12 +24,5 @@ boxplot(measurement$y300~measurement$part,
         main = "Boxplot of Camshaft Straightness by Part",
         xlab = "Part", ylab = "Camshaft Straightness", col = "aquamarine3")
 
-# Measurement Day - use if we stratify over each day, 20 measurements per part over a week, 4 per part over a day
-
-# measurement$day <- c(rep(c(rep(1, 4), rep(2, 4), rep(3, 4), rep(4, 4)),3))
-# 
-# boxplot(measurement$y300~measurement$day, 
-#         main = "Boxplot of Camshaft Straightness by Part",
-#         xlab = "Part", ylab = "Camshaft Straightness", col = "royalblue1")
 
 

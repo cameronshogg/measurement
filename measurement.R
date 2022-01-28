09f9041936b9df203c7d3418f7bec0adedff966c
Cameron Sequeira-Hogg <cameronsequeirahogg@gmail.com>
2022-01-28 15:49:51 -0500

    finalized code

--- measurement.R
+++ measurement.R
@@ -1,15 +1,28 @@
+# Measurement Data
 measurement <- read.csv("measurement.csv", header=T)
 
+# Baseline Data
+inv <- read.csv("Investigation-1.csv", header=T)
+
+# Initial, Baseline Estimates of the Parts
+subset(inv, partnum == 1240)
+subset(inv, partnum == 3447)
+subset(inv, partnum == 2730)
+
 # Variance Stuff
+measurement$part <- c(rep(1, 18), rep(2, 18), rep(3, 18))
 
 part_one = subset(measurement, part == 1)
-var(part_one$y300)
+mean(part_one$y300)
+sqrt(var(part_one$y300))
 
 part_two = subset(measurement, part == 2)
-var(part_two$y300)
+mean(part_two$y300)
+sqrt(var(part_two$y300))
 
 part_three = subset(measurement, part == 3)
-var(part_three$y300)
+mean(part_three$y300)
+sqrt(var(part_three$y300))
 
 #SD 
 sqrt(var(part_one$y300+part_two$y300+part_three$y300)/3)
@@ -20,7 +33,7 @@ sqrt(var(part_one$y300+part_two$y300+part_three$y300)/3)
 
 measurement$part <- c(rep(1, 18), rep(2, 18), rep(3, 18))
 
-boxplot(measurement$y300~measurement$part, 
+boxplot(measurement$y300~measurement$partnum, 
         main = "Boxplot of Camshaft Straightness by Part",
         xlab = "Part", ylab = "Camshaft Straightness", col = "aquamarine3")
 
